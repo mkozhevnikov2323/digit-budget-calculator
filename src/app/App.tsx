b3b6eee59fb86ca04507f02eaee72b5522a32d53
mkozhevnikov2323 <mkozhevnikov23@gmail.com>
2025-05-05 21:14:10 +0400

    add material ui, widget Navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import AppRouter from 'app/providers/router/AppRouter';
+import { Navbar } from 'widgets/Navbar';
 
 export const App: React.FC = (): React.ReactNode => {
   return (
     <>
+      <Navbar />
       <AppRouter />
     </>
   );
